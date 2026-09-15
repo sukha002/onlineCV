162fa75e076816995a4ea2540f66a04fbf6fae16
Sukhwinder Singh <sukhsinghturka02@gmail.com>
2026-09-15 07:08:54 -0400

    Fix Django views conflict

--- main/views.py
+++ main/views.py
@@ -4,12 +4,8 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "main/home.html")
-    
+
+
 def about(request):
     text = "Hello, this text is returned in the Django view!"
     return HttpResponse(text)
-<<<<<<< HEAD
-=======
-
-
->>>>>>> c93260d (Prepare Django app for Azure)
