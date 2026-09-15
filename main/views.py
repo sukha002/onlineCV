721b464195279f2007b1c25b72c5ace73847c3bf
Sukhwinder Singh <sukhsinghturka02@gmail.com>
2026-09-15 06:38:08 -0400

    Initial Django portfolio

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "main/home.html")
+    
+def about(request):
+    text = "Hello, this text is returned in the Django view!"
+    return HttpResponse(text)
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> c93260d (Prepare Django app for Azure)
